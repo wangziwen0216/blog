cf8025be2d2a5c5b42fcf93712ab1198afb726d1
Wisdom Wang <317689136@qq.com>
2018-12-29 11:05:40 +0800

    user model and controllers

---
+++ app/Model/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    //
+    public $primaryKey = 'user_id';
+
+    public $timestamps = false;
+
+
+}
